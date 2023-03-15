19e4e6b0ef19e32903b641807a7b645b6b1ddcc4
unknown <asmachloe@gmail.com>
2023-03-15 19:13:57 +0100

    prout

--- interface/main.py
+++ interface/main.py
@@ -7,6 +7,7 @@ from PyQt5.QtCore import Qt
 from keras_preprocessing.image import load_img, img_to_array, array_to_img
 
 from augmentation import fonctions
+from interface import range_slider
 
 
 class Interface(QWidget):
@@ -14,6 +15,7 @@ class Interface(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.sl = None
         self.status_label = None
         self.folder_path = ''
         self.list_widget = None
@@ -74,7 +76,7 @@ class Interface(QWidget):
         self.transformation_selector.setGeometry(10, 350, 580, 30)
         self.transformation_selector.setStyleSheet("background-color: white; border: 1px solid #DDDDDD;"
                                                    "font-size: 14px; padding-left: 5px;")
-        self.transformation_selector.currentIndexChanged.connect(self.resetUi)
+        self.transformation_selector.currentIndexChanged.connect(self.on_Index_changed)
 
         # Ajout du bouton "Modifier"
         modifier_button = QPushButton("Modifier", self)
@@ -86,6 +88,8 @@ class Interface(QWidget):
         tout_cocher_button = QPushButton("Tout cocher", self)
         tout_cocher_button.setGeometry(10, 600, 580, 30)
         tout_cocher_button.clicked.connect(self.on_cocher_button_clicked)
+
+        self.on_Index_changed()
 
         self.show()
 
@@ -149,11 +153,37 @@ class Interface(QWidget):
         for child in self.children():
             if isinstance(child, QLabel) and child.text() != "Files in selected folder:":
                 child.deleteLater()
-
-        # Supprimer les boutons "Modifier" et "Enregistrer"
-        for child in self.children():
-            if isinstance(child, QPushButton) and child.text() == ["Modifier","Enregistrer"]:
+            if isinstance(child, QPushButton) and child.text() == "Enregistrer":
                 child.deleteLater()
+
+    def on_Index_changed(self):
+        self.resetUi()
+        selected_transform = self.transformation_selector.currentText()
+
+        if selected_transform == "Zoom":
+            self.on_Zoom_index()
+        elif selected_transform == "Translate":
+            self.on_Translate_index()
+        elif selected_transform == "Flip":
+            self.on_Flip_index()
+        elif selected_transform == "Rotate":
+            self.on_Rotate_index()
+        elif selected_transform == "Brightness":
+            self.on_Brightness_index()
+        elif selected_transform == "Resize":
+            self.on_Resize_index()
+        elif selected_transform == "Scale":
+            self.on_Scale_index()
+        elif selected_transform == "Blur":
+            self.on_Blur_index()
+        elif selected_transform == "Noise":
+            self.on_Noise_index()
+        elif selected_transform == "Contrast":
+            self.on_Contrast_index()
+        elif selected_transform == "Crop":
+            self.on_Crop_index()
+        else:
+            self.handle_error("Invalid transformation selected.")
 
     def on_Zoom_clicked(self):
         if len(self.get_checked_items()) == 0:
@@ -167,18 +197,18 @@ class Interface(QWidget):
             img = load_img(path)
             # conversion en numpy array
             data = img_to_array(img)
-            liste = fonctions.zoom(image=data, height_range=(0.5, 0.5), width_range=(0.5, 0.5), n=2)
+            liste = fonctions.zoom(image=data, height_range=(-1, 1), width_range=(-1, 1), n=2, fill_mode='constant')
             for j in liste:
                 zoomed_images.append(j)
 
         # Ajouter un QLabel pour afficher le message
         message_label = QLabel("Images modifiées!", self)
-        message_label.setGeometry(10, 450, 580, 30)
+        message_label.setGeometry(10, 490, 580, 30)
         message_label.show()
 
         # Ajouter un bouton "Enregistrer"
         save_button = QPushButton("Enregistrer", self)
-        save_button.setGeometry(10, 490, 100, 30)
+        save_button.setGeometry(10, 520, 100, 30)
         print(zoomed_images)
         save_button.clicked.connect(lambda: self.save_images(zoomed_images))
         save_button.show()
@@ -211,6 +241,69 @@ class Interface(QWidget):
         pass
 
     def on_Crop_clicked(self):
+        pass
+
+    # Modification du sélecteur de transformation
+
+    def on_Zoom_index(self):
+
+        # Créer un QLabel pour afficher le texte "Zoom Level :"
+        zoom_label = QLabel("Height_range :", self)
+        zoom_label.setGeometry(10, 400, 80, 30)
+
+        slider = range_slider.RangeSlider(Qt.Horizontal)
+        slider.setMinimumHeight(30)
+        slider.setMinimum(0)
+        slider.setMaximum(255)
+        slider.setLow(15)
+        slider.setHigh(35)
+        slider.setGeometry(100, 400, 200, 30)
+        slider.sliderMoved.connect(self.update_value_label)
+        # QtCore.QObject.connect(slider, QtCore.SIGNAL('sliderMoved(int)'), echo)
+        slider.show()
+        slider.raise_()
+
+        # Ajouter un QLabel pour afficher la valeur actuelle du slider
+        value_label = QLabel(f"{self.sl.value()}", self)
+        value_label.setGeometry(100, 400, 50, 30)
+        value_label.setStyleSheet("font-size: 14px;")
+
+        # Connecter le signal valueChanged du slider avec la fonction update_value_label
+        self.sl.valueChanged.connect(lambda: self.update_value_label(value_label))
+
+        self.show()
+
+    def update_value_label(self, label):
+        label.setText(f"{self.sl.value()}")
+
+    def on_Translate_index(self):
+        pass
+
+    def on_Flip_index(self):
+        pass
+
+    def on_Rotate_index(self):
+        pass
+
+    def on_Brightness_index(self):
+        pass
+
+    def on_Resize_index(self):
+        pass
+
+    def on_Scale_index(self):
+        pass
+
+    def on_Blur_index(self):
+        pass
+
+    def on_Noise_index(self):
+        pass
+
+    def on_Contrast_index(self):
+        pass
+
+    def on_Crop_index(self):
         pass
 
 
